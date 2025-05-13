b51fedb1699ceb9c2c3d936950e686d046b7657b
thinhnguyen <thinhnguyen14.ptit@gmail.com>
2025-05-13 23:52:55 +0700

    Thuc_Hanh_Tren_Lop

--- Thinh Nguyen-B24DCCC255-JS04/2025_05_09/Thuc_Hanh_Tren_Lop/th.js
+++ Thinh Nguyen-B24DCCC255-JS04/2025_05_09/Thuc_Hanh_Tren_Lop/th.js
@@ -1,5 +1,46 @@
-for (var i = 1, i <= 100, i += i) 
-    if (i > 100) {
-        console.log(i);{
+// bai1:
+// cach 1: dùng for 
+var tongFor = 0;
+for (var iFor = 1; iFor <= 100; iFor++) {
+    tongFor += iFor;
+    if (tongFor >= 100) {
+        console.log(`Tổng (for): ${tongFor} và Số lần lặp (for): ${iFor} `);
+        break;
     }
 }
+// cach 2: dùng while
+var tongWhile = 0;
+var iWhile = 1;
+while (iWhile <=100) {
+    iWhile++;
+    tongWhile += iWhile;
+    if (tongWhile >= 100) {
+        console.log(`Tổng (while): ${tongWhile} và Số lần lặp (while): ${iWhile}`);
+        break;
+    }
+}
+
+// bai2:
+var n;
+    while (true) {
+        var input = prompt("Vui lòng nhập một số lớn hơn 0:");
+        n = parseInt(input);
+
+        // Kiểm tra xem n có phải là một số và lớn hơn 0 hay không
+        if (typeof n === 'number' && n > 0) {
+            break; // Thoát khỏi vòng lặp
+        } else if (input === null) {
+            alert("Bạn đã hủy nhập.");
+            break;
+        } else {
+            alert("Số bạn nhập không hợp lệ. Vui lòng nhập lại số lớn hơn 0.");
+        }
+    }
+
+    if (n !== undefined) {
+        if (n % 2 === 0) {
+            alert(`${n} là số chẵn.`);
+        } else {
+            alert(`${n} là số lẻ.`);
+        }
+    }
